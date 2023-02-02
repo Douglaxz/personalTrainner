332332c2820eedb5026d64c02aea4d1b8ce83427
Douglas Amaral <douglaxz@hotmail.com>
2023-02-02 09:34:05 -0300

    correções nas rotas

--- views.py
+++ views.py
@@ -62,3 +62,281 @@ def autenticar():
         flash('Usuário não logado com sucesso','success')
         return redirect(url_for('login'))
 
+#---------------------------------------------------------------------------------------------------------------------------------
+#USUARIOS
+#---------------------------------------------------------------------------------------------------------------------------------
+
+# rota index para mostrar os usuários
+@app.route('/usuario', methods=['POST','GET'])
+def usuario():
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        flash('Sessão expirou, favor logar novamente','danger')
+        return redirect(url_for('login',proxima=url_for('usuario')))        
+    form = FormularPesquisa()
+    page = request.args.get('page', 1, type=int)
+    pesquisa = form.pesquisa.data
+    if pesquisa == "":
+        pesquisa = form.pesquisa_responsiva.data
+
+    if pesquisa == "" or pesquisa == None:    
+        usuarios = tb_user.query\
+        .join(tb_usertype, tb_usertype.cod_usertype==tb_user.cod_usertype)\
+        .add_columns(tb_user.login_user, tb_user.cod_user, tb_user.name_user, tb_user.status_user, tb_usertype.desc_usertype)\
+        .order_by(tb_user.name_user)\
+        .paginate(page=page, per_page=ROWS_PER_PAGE, error_out=False)
+    else:
+        usuarios = tb_user.query\
+        .filter(tb_user.name_user.ilike(f'%{pesquisa}%'))\
+        .join(tb_usertype, tb_usertype.cod_usertype==tb_user.cod_usertype)\
+        .add_columns(tb_user.login_user, tb_user.cod_user, tb_user.name_user, tb_user.status_user, tb_usertype.desc_usertype)\
+        .order_by(tb_user.name_user)\
+        .paginate(page=page, per_page=ROWS_PER_PAGE, error_out=False)
+
+
+    return render_template('usuarios.html', titulo='Usuários', usuarios=usuarios, form=form)
+
+# rota para criar novo formulário usuário 
+@app.route('/novoUsuario')
+def novoUsuario():
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        flash('Sessão expirou, favor logar novamente','danger')
+        return redirect(url_for('login',proxima=url_for('novoUsuario')))     
+    form = FormularioUsuario()
+    return render_template('novoUsuario.html', titulo='Novo Usuário', form=form)
+
+# rota para visualizar usuário 
+@app.route('/visualizarUsuario/<int:id>')
+def visualizarUsuario(id):
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        flash('Sessão expirou, favor logar novamente','danger')
+        return redirect(url_for('login',proxima=url_for('visualizarUsuario')))    
+    usuario = tb_user.query.filter_by(cod_user=id).first()
+    form = FormularioUsuarioVisualizar()
+    form.nome.data = usuario.name_user
+    form.status.data = usuario.status_user
+    form.login.data = usuario.login_user
+    form.tipousuario.data = usuario.cod_usertype
+    form.email.data = usuario.email_user
+    return render_template('visualizarUsuario.html', titulo='Visualizar Usuário', id=id, form=form)   
+
+# rota para editar formulário usuário 
+@app.route('/editarUsuario/<int:id>')
+def editarUsuario(id):
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        flash('Sessão expirou, favor logar novamente','danger')
+        return redirect(url_for('login',proxima=url_for('editarUsuario/<int:id>')))  
+    usuario = tb_user.query.filter_by(cod_user=id).first()
+    form = FormularioUsuario()
+    form.nome.data = usuario.name_user
+    form.status.data = usuario.status_user
+    form.login.data = usuario.login_user
+    form.tipousuario.data = usuario.cod_usertype
+    form.email.data = usuario.email_user
+    return render_template('editarUsuario.html', titulo='Editar Usuário', id=id, form=form)    
+       
+# rota para criar usuário no banco de dados
+@app.route('/criarUsuario', methods=['POST',])
+def criarUsuario():
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        return redirect(url_for('login',proxima=url_for('criarUsuario')))      
+    form = FormularioUsuario(request.form)
+    if not form.validate_on_submit():
+        flash('Por favor, preencha todos os dados','danger')
+        return redirect(url_for('novoUsuario'))
+    nome  = form.nome.data
+    status = form.status.data
+    login = form.login.data
+    tipousuario = form.tipousuario.data
+    email = form.email.data
+    #criptografar senha
+    senha = generate_password_hash("teste@12345").decode('utf-8')
+    usuario = tb_user.query.filter_by(name_user=nome).first()
+    if usuario:
+        flash ('Usuário já existe','danger')
+        return redirect(url_for('index')) 
+    novoUsuario = tb_user(name_user=nome, status_user=status, login_user=login, cod_usertype=tipousuario, password_user=senha, email_user=email)
+    db.session.add(novoUsuario)
+    db.session.commit()
+    flash('Usuário criado com sucesso','success')
+    return redirect(url_for('usuario'))
+
+# rota para criar usuário no banco de dados
+@app.route('/criarUsuarioexterno', methods=['POST',])
+def criarUsuarioexterno():    
+    nome  = request.form['nome']
+    status = 0
+    email = request.form['email']
+    localarroba = email.find("@")
+    login = email[0:localarroba]
+    tipousuario = 2
+    
+    #criptografar senha
+    senha = generate_password_hash(request.form['senha']).decode('utf-8')
+
+    usuario = tb_user.query.filter_by(name_user=nome).first()
+    if usuario:
+        flash ('Usuário já existe','danger')
+        return redirect(url_for('login')) 
+    novoUsuario = tb_user(name_user=nome, status_user=status, login_user=login, cod_usertype=tipousuario, password_user=senha, email_user=email)
+    db.session.add(novoUsuario)
+    db.session.commit()
+    flash('Usuário criado com sucesso, favor logar com ele','success')
+    return redirect(url_for('login'))    
+
+# rota para atualizar usuário no banco de dados
+@app.route('/atualizarUsuario', methods=['POST',])
+def atualizarUsuario():
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        flash('Sessão expirou, favor logar novamente','danger')
+        return redirect(url_for('login',proxima=url_for('atualizarUsuario')))          
+    form = FormularioUsuario(request.form)
+    if not form.validate_on_submit():
+        flash('Por favor, preencha todos os dados','danger')
+        return redirect(url_for('atualizarUsuario'))
+    id = request.form['id']
+    usuario = tb_user.query.filter_by(cod_user=request.form['id']).first()
+    usuario.name_user = form.nome.data
+    usuario.status_user = form.status.data
+    usuario.login_user = form.login.data
+    usuario.cod_uertype = form.tipousuario.data
+    db.session.add(usuario)
+    db.session.commit()
+    flash('Usuário alterado com sucesso','success')
+    return redirect(url_for('visualizarUsuario', id=id))
+
+# rota para visualizar usuário 
+@app.route('/editarSenhaUsuario/')
+def editarSenhaUsuario():
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        flash('Sessão expirou, favor logar novamente','danger')
+        return redirect(url_for('login',proxima=url_for('visualizarUsuario')))    
+    form = FormularioUsuarioTrocarSenha()
+    return render_template('trocarsenha.html', titulo='Trocar Senha', id=id, form=form)  
+
+# rota para atualizar usuário no banco de dados
+@app.route('/trocarSenhaUsuario', methods=['POST',])
+def trocarSenhaUsuario():
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        flash('Sessão expirou, favor logar novamente','danger')
+        return redirect(url_for('login',proxima=url_for('atualizarUsuario')))          
+    form = FormularioUsuarioTrocarSenha(request.form)
+    if form.validate_on_submit():
+        id = session['coduser_logado']
+        usuario = tb_user.query.filter_by(cod_user=id).first()
+        if form.senhaatual.data != usuario.password_user:
+            flash('senha atual incorreta','danger')
+            return redirect(url_for('editarSenhaUsuario'))
+
+        if form.senhaatual.data != usuario.password_user:
+            flash('senha atual incorreta','danger')
+            return redirect(url_for('editarSenhaUsuario')) 
+
+        if form.novasenha1.data != form.novasenha2.data:
+            flash('novas senhas não coincidem','danger')
+            return redirect(url_for('editarSenhaUsuario')) 
+        usuario.password_user = generate_password_hash(form.novasenha1.data).decode('utf-8')
+        db.session.add(usuario)
+        db.session.commit()
+        flash('senha alterada com sucesso!','success')
+    else:
+        flash('senha não alterada!','danger')
+    return redirect(url_for('editarSenhaUsuario')) 
+
+#---------------------------------------------------------------------------------------------------------------------------------
+#TIPO USUARIOS
+#---------------------------------------------------------------------------------------------------------------------------------
+
+# rota index para mostrar os tipo usuários
+@app.route('/tipousuario', methods=['POST','GET'])
+def tipousuario():
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        flash('Sessão expirou, favor logar novamente','danger')
+        return redirect(url_for('login',proxima=url_for('tipousuario')))         
+    page = request.args.get('page', 1, type=int)
+    form = FormularPesquisa()   
+    pesquisa = form.pesquisa.data
+    if pesquisa == "":
+        pesquisa = form.pesquisa_responsiva.data
+    
+    if pesquisa == "" or pesquisa == None:     
+        tiposusuario = tb_usertype.query.order_by(tb_usertype.desc_usertype)\
+        .paginate(page=page, per_page=ROWS_PER_PAGE , error_out=False)
+    else:
+        tiposusuario = tb_usertype.query.order_by(tb_usertype.desc_usertype)\
+        .filter(tb_usertype.desc_usertype.ilike(f'%{pesquisa}%'))\
+        .paginate(page=page, per_page=ROWS_PER_PAGE, error_out=False)        
+    return render_template('tipousuarios.html', titulo='Tipo Usuário', tiposusuario=tiposusuario, form=form)
+
+# rota para criar novo formulário usuário 
+@app.route('/novoTipoUsuario')
+def novoTipoUsuario():
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        flash('Sessão expirou, favor logar novamente','danger')
+        return redirect(url_for('login',proxima=url_for('novoTipoUsuario'))) 
+    form = FormularioTipoUsuarioEdicao()
+    return render_template('novoTipoUsuario.html', titulo='Novo Tipo Usuário', form=form)
+
+# rota para criar tipo usuário no banco de dados
+@app.route('/criarTipoUsuario', methods=['POST',])
+def criarTipoUsuario():
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        flash('Sessão expirou, favor logar novamente','danger')
+        return redirect(url_for('login',proxima=url_for('criarTipoUsuario')))     
+    form = FormularioTipoUsuarioEdicao(request.form)
+    if not form.validate_on_submit():
+        flash('Por favor, preencha todos os dados','danger')
+        return redirect(url_for('criarTipoUsuario'))
+    desc  = form.descricao.data
+    status = form.status.data
+    tipousuario = tb_usertype.query.filter_by(desc_usertype=desc).first()
+    if tipousuario:
+        flash ('Tipo Usuário já existe','danger')
+        return redirect(url_for('tipousuario')) 
+    novoTipoUsuario = tb_usertype(desc_usertype=desc, status_usertype=status)
+    flash('Tipo de usuário criado com sucesso!','success')
+    db.session.add(novoTipoUsuario)
+    db.session.commit()
+    return redirect(url_for('tipousuario'))
+
+# rota para visualizar tipo usuário 
+@app.route('/visualizarTipoUsuario/<int:id>')
+def visualizarTipoUsuario(id):
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        flash('Sessão expirou, favor logar novamente','danger')
+        return redirect(url_for('login',proxima=url_for('visualizarTipoUsuario')))  
+    tipousuario = tb_usertype.query.filter_by(cod_usertype=id).first()
+    form = FormularioTipoUsuarioVisualizar()
+    form.descricao.data = tipousuario.desc_usertype
+    form.status.data = tipousuario.status_usertype
+    return render_template('visualizarTipoUsuario.html', titulo='Visualizar Tipo Usuário', id=id, form=form)   
+
+# rota para editar formulário tipo usuário 
+@app.route('/editarTipoUsuario/<int:id>')
+def editarTipoUsuario(id):
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        flash('Sessão expirou, favor logar novamente','danger')
+        return redirect(url_for('login',proxima=url_for('editarTipoUsuario')))  
+    tipousuario = tb_usertype.query.filter_by(cod_usertype=id).first()
+    form = FormularioTipoUsuarioEdicao()
+    form.descricao.data = tipousuario.desc_usertype
+    form.status.data = tipousuario.status_usertype
+    return render_template('editarTipoUsuario.html', titulo='Editar Tipo Usuário', id=id, form=form)   
+
+# rota para atualizar usuário no banco de dados
+@app.route('/atualizarTipoUsuario', methods=['POST',])
+def atualizarTipoUsuario():
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        flash('Sessão expirou, favor logar novamente','danger')
+        return redirect(url_for('login',proxima=url_for('atualizarTipoUsuario')))      
+    form = FormularioTipoUsuarioEdicao(request.form)
+    if form.validate_on_submit():
+        id = request.form['id']
+        tipousuario = tb_usertype.query.filter_by(cod_usertype=request.form['id']).first()
+        tipousuario.desc_usertype = form.descricao.data
+        tipousuario.status_usertype = form.status.data
+        db.session.add(tipousuario)
+        db.session.commit()
+        flash('Tipo de usuário atualizado com sucesso!','success')
+    else:
+        flash('Favor verificar os campos!','danger')
+    return redirect(url_for('visualizarTipoUsuario', id=id))    
